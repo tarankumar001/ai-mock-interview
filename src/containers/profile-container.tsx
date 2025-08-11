fa231cb41dc77d7bee23716c211cc9104fd985f6
Taran Kumar <140129581+tarankumar001@users.noreply.github.com>
2025-08-11 23:50:55 +0530

    implemented profile

--- src/containers/profile-container.tsx
+++ src/containers/profile-container.tsx
@@ -1,6 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { useAuth, UserButton } from "@clerk/clerk-react";
+import { Loader } from "lucide-react";
+import { Link } from "react-router-dom"; // ✅ Correct import
 
 export const ProfileContainer = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <div className="flex items-center justify-center h-screen gap-6">
+        <Loader className="min-w-4 min-h-4 animate-spin text-emerald-500" />
+      </div>
+    );
+  }
+
   return (
-    <div>ProfileContainer</div>
-  )
-}
+    <div className="flex items-center placeholder-gray-400 gap-4">
+      {isSignedIn ? (
+        <UserButton afterSignOutUrl="/" />
+      ) : (
+        <>
+          <Link to="/sign-in">
+            
+            <Button size="sm" variant="outline">Login</Button>
+          </Link>
+          <Link to="/sign-up">
+            
+            <Button size="sm">Get Started</Button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
