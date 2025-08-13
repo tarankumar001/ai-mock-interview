0d9156b24a985c941a90c21c4fa1cfa252c45a50
Taran Kumar <140129581+tarankumar001@users.noreply.github.com>
2025-08-13 21:10:43 +0530

    headingg in dashboared

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,7 +1,15 @@
-
+import { Headings } from "@/components/ui/headings"
 
 export const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+  return <>
+  <div className="flex w-full items-center justify-between">
+    
+    
+    {/* heading */}
+    <Headings title="Dashboard" description="Create and start your AI Mock Interview" />
+    
+    </div>
+  </>
+    
+
 }
