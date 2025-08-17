34d278a09da1a256206ad099f34ca82eb023e67b
Taran Kumar <140129581+tarankumar001@users.noreply.github.com>
2025-08-17 10:05:19 +0530

    logo change

--- src/components/logo-container.tsx
+++ src/components/logo-container.tsx
@@ -4,9 +4,9 @@ export const LogoContainer = () => {
   return (
     <Link to={"/"}>
       <img
-        src="/svg/logo.svg"
-        alt=""
-  className="min-w-12 min-h-12 w-12 h-12 object-contain"
+        src="/img/logo/logo.jpg"
+        alt="PTK Creative Logo"
+        className="min-w-12 min-h-12 w-12 h-12 object-contain rounded-md"
       />
     </Link>
   );
